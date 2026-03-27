aa35fd2e265cde28e985505ed11e5b190eace8a2
Sam Bonnekamp <sam@bonnekamp.net>
2026-03-27 16:57:18 +0800

    shape API

--- write.c
+++ write.c
@@ -14,6 +14,13 @@ typedef struct{
   uint8_t B;
 }rgb_pixel;
 
+typedef struct{
+  uint64_t x;
+  uint64_t y;
+}pixel_coord;
+
+void square(pixel_coord center, uint32_t size, uint32_t column_size, rgb_pixel *buffer, rgb_pixel colour);
+
 int main(int argc, char* argv[]){
   assert(sizeof(rgb_pixel) == 3);
   rgb_pixel *buffer;
@@ -24,6 +31,8 @@ int main(int argc, char* argv[]){
   image.format = PNG_FORMAT_RGB;
   image.width = 720;
   image.height = 720;
+
+  uint16_t column_size = image.width;
 
   size_t buffer_size = image.width * image.height * sizeof(rgb_pixel);
   buffer = mmap(NULL, buffer_size,
@@ -37,22 +46,30 @@ int main(int argc, char* argv[]){
     printf("address: %p\nsize: %ld\n", buffer, buffer_size);
   }
 
-  int erm_pause = (image.width * image.height)/255;
-  int decrease_by = 0;
-  printf("pause: %d\n", erm_pause);
+  memset(buffer, 0xFF, buffer_size);
 
-  for(int i = 0; i < image.width * image.height; i++){
+  //square
+  pixel_coord center = {image.width>>1, image.height>>1};
+  pixel_coord center2 = {image.width>>2, image.height>>2};
+  uint32_t size = 50;
+  square(center, size, column_size, buffer, (rgb_pixel){0x00, 0xAA, 0xB7});
+  square(center2, 25, column_size, buffer, (rgb_pixel){0x00, 0x00, 0x00});
 
-    if(i % erm_pause == 0) decrease_by++;
-
-    buffer[i].R = 0xFF - decrease_by;
-    buffer[i].G = 0xFF - decrease_by;
-    buffer[i].B = 0xFF - decrease_by;
-  }
   if(png_image_write_to_file(&image, "output.png", 0, buffer, 0, NULL) == 0 ){
     perror("failed to write");
     return 1;
   }
-  printf("final decrease: %d\n", decrease_by);
+
   return 0;
+}
+
+void square(pixel_coord center, uint32_t size, uint32_t column_size, rgb_pixel *buffer, rgb_pixel colour){
+  uint32_t start_height = (center.y-(size>>1)) * column_size;
+  uint32_t end_height = (center.y+(size>>1)) * column_size;
+
+  for(uint32_t i = start_height; i < end_height; i+= column_size){
+    for(uint32_t j = center.x - (size>>1); j < center.x + (size>>1); j++){
+      buffer[i+j] = colour;
+    }
+  }
 }
