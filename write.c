79903da3c9c915aef3ddce1958411232c3b83bad
Sam Bonnekamp <sam@bonnekamp.net>
2026-03-30 22:51:06 +0800

    refactored out some arguments into context variable

--- write.c
+++ write.c
@@ -22,29 +22,35 @@ typedef struct{
 typedef struct{
   uint32_t width;
   uint32_t height;
+  pixel_coord center;
   rgb_pixel bg_colour;
+  rgb_pixel **buffer;
 }image_ctx;
 
 double pythagoras(double length, double width);
-void square(pixel_coord center, uint32_t size, uint32_t column_size, rgb_pixel *buffer, rgb_pixel colour);
-void circle(pixel_coord center, double radius, uint32_t column_size, rgb_pixel *buffer, rgb_pixel colour);
-void linear(double gradient, double constant, rgb_pixel **buffer, rgb_pixel colour, pixel_coord center);
-void vertical_line(rgb_pixel **buffer, rgb_pixel colour, pixel_coord center);
+void square(pixel_coord center, uint32_t size, rgb_pixel colour, image_ctx *ctx);
+void circle(pixel_coord center, double radius, rgb_pixel colour, image_ctx *ctx);
+void linear(double gradient, double constant, rgb_pixel colour, image_ctx *ctx);
+void vertical_line(rgb_pixel colour, image_ctx *ctx);
 
 int main(int argc, char* argv[]){
+  image_ctx ctx = {720, 720, {720/2, 720/2}, {0xFF, 0xFF, 0xFF}, NULL};
   assert(sizeof(rgb_pixel) == 3);
   rgb_pixel *buffer;
   png_image image;
+  rgb_pixel *buffer_2d[ctx.width];
+  uint16_t column_size = ctx.width;
+  const size_t buffer_size = ctx.width * ctx.height * sizeof(rgb_pixel);
+
+  //libpng
   memset(&image, 0, sizeof(image));
   image.version = PNG_IMAGE_VERSION;
   image.opaque = NULL; //demanded by the documentation for reasons I don't get
   image.format = PNG_FORMAT_RGB;
-  image.width = 720;
-  image.height = 720;
-  rgb_pixel *buffer_2d[image.width];
+  image.width = ctx.width;
+  image.height = ctx.height;
 
-  uint16_t column_size = image.width;
-  const size_t buffer_size = image.width * image.height * sizeof(rgb_pixel);
+  ctx.buffer = buffer_2d;
 
   buffer = mmap(NULL, buffer_size,
                 PROT_READ|PROT_WRITE,
@@ -55,42 +61,48 @@ int main(int argc, char* argv[]){
     return 1;
   }
 
+  //populate Y axis
   for(uint32_t i = 0; i < image.width; i++){
-    buffer_2d[i] = buffer + (column_size*i);
+    ctx.buffer[i] = buffer + (column_size*i);
   }
 
-  memset(buffer, 0xFF, buffer_size);
-  /*
+  //populate image with bg colour
+  for(uint32_t i = 0; i < ctx.width * ctx.height; i++){
+    buffer[i] = ctx.bg_colour;
+  }
+
+
   //square
+  /*
   pixel_coord center = {image.width>>1, image.height>>1};
   uint32_t size = 50;
-  square(center, size, column_size, buffer, (rgb_pixel){0x00, 0xAA, 0xB7});
+  square(center, size, (rgb_pixel){0x00, 0xAA, 0xB7}, &ctx);
 
 
   //circle
   pixel_coord center2 = {image.width>>2, image.height>>2};
   double radius = 20;
   rgb_pixel colour = (rgb_pixel){0x00, 0xAA, 0xB7};
-  circle(center2, radius, column_size, buffer, colour);
+  circle(center2, radius, colour, &ctx);
   */
 
   //line
   rgb_pixel black = {0x00, 0x00, 0x00};
-  linear(0, 1, buffer_2d, black, (pixel_coord){image.width/2, image.height/2});
-  linear(0, 0, buffer_2d, black, (pixel_coord){image.width/2, image.height/2});
-  linear(0, -1, buffer_2d, black, (pixel_coord){image.width/2, image.height/2});
+  linear(0, 1, black, &ctx);
+  linear(0, 0, black, &ctx);
+  linear(0, -1, black, &ctx);
 
-  vertical_line(buffer_2d, black, (pixel_coord){image.width/2, image.height/2});
+  vertical_line(black, &ctx);
 
   rgb_pixel nice_blue = {0x00, 0xAA, 0xB7};
   rgb_pixel dithered_nice_blue = {nice_blue.R + ((0xFF - nice_blue.R)*0.5),
                                   nice_blue.G + ((0xFF - nice_blue.G)*0.5),
                                   nice_blue.B + ((0xFF - nice_blue.B)*0.5)};
-  linear(0.5, 0, buffer_2d, nice_blue, (pixel_coord){image.width/2, image.height/2});
-  linear(0.5, 1, buffer_2d, nice_blue, (pixel_coord){image.width/2, image.height/2});
-  linear(0.5, -1, buffer_2d, nice_blue, (pixel_coord){image.width/2, image.height/2});
-  linear(0.5, 2, buffer_2d, dithered_nice_blue, (pixel_coord){image.width/2, image.height/2});
-  linear(0.5, -2, buffer_2d, dithered_nice_blue, (pixel_coord){image.width/2, image.height/2});
+  linear(0.5, 1, nice_blue, &ctx);
+  linear(0.5, 0, nice_blue, &ctx);
+  linear(0.5, -1, nice_blue, &ctx);
+  linear(0.5, 2, dithered_nice_blue, &ctx);
+  linear(0.5, -2, dithered_nice_blue, &ctx);
 
 
   if(png_image_write_to_file(&image, "output.png", 0, buffer, 0, NULL) == 0 ){
@@ -110,35 +122,33 @@ double pythagoras(double length, double width){
 
 }
 
-void vertical_line(rgb_pixel **buffer, rgb_pixel colour, pixel_coord center){
+void vertical_line(rgb_pixel colour, image_ctx *ctx){
 
   for(uint16_t i = 0; i < 720; i++){
-    buffer[i][center.x] = colour;
-    buffer[i][center.x+1] = colour;
-    buffer[i][center.x-1] = colour;
+    ctx->buffer[i][ctx->center.x] = colour;
+    ctx->buffer[i][(ctx->center.x)+1] = colour;
+    ctx->buffer[i][(ctx->center.x)-1] = colour;
   }
 }
 
-void linear(double gradient, double constant, rgb_pixel **buffer, rgb_pixel colour, pixel_coord center){
-  double x = -center.y;
+void linear(double gradient, double constant, rgb_pixel colour, image_ctx *ctx){
+  double x = -ctx->center.y;
   double y = 0;
 
-  for(; x < center.x; x++){
+  for(; x < ctx->center.x; x++){
     y = (gradient*x*-1)-constant;
-    if(y >= -center.y && y <= center.y)
-      buffer[(uint32_t)y+center.y][(uint32_t)x+center.x] = colour;
+    if(y >= -ctx->center.y && y <= ctx->center.y)
+      ctx->buffer[(uint32_t)y+ctx->center.y][(uint32_t)x+ctx->center.x] = colour;
   }
 }
 
-void circle(pixel_coord center, double radius, uint32_t column_size, rgb_pixel *buffer, rgb_pixel colour){
-  double start_height = (center.y-radius) * column_size;
-  double end_height = (center.y+radius) * column_size;
+void circle(pixel_coord center, double radius, rgb_pixel colour, image_ctx *ctx){
 
-  for(double i = start_height; i <= end_height; i+= column_size){
+  for(double i = center.y - radius; i <= center.y + radius; i++){
     for(double j = center.x - radius; j <= center.x + radius; j++){
-      double distance = pythagoras((i/column_size)-center.y, j-center.x);
+      double distance = pythagoras(i-center.y, j-center.x);
       if(distance < radius)
-        buffer[(int)(i+j)] = colour;
+        ctx->buffer[(int)i][(int)j] = colour;
       else{
         double fade_width = 1.2;
         double fade_aggression = 2;
@@ -146,19 +156,17 @@ void circle(pixel_coord center, double radius, uint32_t column_size, rgb_pixel *
         rgb_pixel colour_diff = {colour.R + (uint8_t)((0xFF-colour.R)*vector_scale),
                                  colour.G + (uint8_t)((0xFF-colour.G)*vector_scale),
                                  colour.B + (uint8_t)((0xFF-colour.B)*vector_scale)};
-        buffer[(int)(i+j)] = colour_diff;
+        ctx->buffer[(int)i][(int)j] = colour_diff;
       }
     }
   }
 }
 
-void square(pixel_coord center, uint32_t size, uint32_t column_size, rgb_pixel *buffer, rgb_pixel colour){
-  uint32_t start_height = (center.y-(size>>1)) * column_size;
-  uint32_t end_height = (center.y+(size>>1)) * column_size;
+void square(pixel_coord center, uint32_t size, rgb_pixel colour, image_ctx *ctx){
 
-  for(uint32_t i = start_height; i < end_height; i+= column_size){
-    for(uint32_t j = center.x - (size>>1); j < center.x + (size>>1); j++){
-      buffer[i+j] = colour;
+  for(uint32_t i = center.y-(size/2); i < center.y+(size/2); i++){
+    for(uint32_t j = center.x-(size/2); j < center.x+(size/2); j++){
+      ctx->buffer[i][j] = colour;
     }
   }
 }
